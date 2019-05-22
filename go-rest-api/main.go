ec43ffac8cf474b96c7d05fc935bbc70ae560465
Chanchal <ckmandal9@gmail.com>
2019-05-23 02:58:26 +0530

    Golang RESTful API using GORM and Gorilla Mux

---
+++ go-rest-api/main.go
@@ -0,0 +1,14 @@
+package main
+ 
+import (
+	"github.com/go-rest-api/app"
+	"github.com/go-rest-api/config"
+)
+ 
+func main() {
+	config := config.GetConfig()
+ 
+	app := &app.App{}
+	app.Initialize(config)
+	app.Run(":3003")
+}
